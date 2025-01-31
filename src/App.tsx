ef3decac67e8288c1ffef5eef28d5647f8982761
Uchenna99 <uchennaagbu@gmail.com>
2025-01-31 10:50:56 +0100

    setting up enviroment

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import LandingPage from './Pages/LandingPage'
 
 function App() {
 
   return (
     <>
+      <Routes>
+        <Route path='/' element={<LandingPage/>} />
+      </Routes>
     </>
   )
 }
