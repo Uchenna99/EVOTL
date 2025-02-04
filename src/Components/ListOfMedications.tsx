7c5f830e9ddd7ec7b4aa189afb092808948f3d8c
Uchenna99 <uchennaagbu@gmail.com>
2025-02-04 17:32:00 +0100

    working on meds list

--- src/Components/ListOfMedications.tsx
+++ src/Components/ListOfMedications.tsx
@@ -4,6 +4,23 @@ const ListOfMedications = () => {
     <>
         <div className="meds-display">
             <p>Select Medications</p>
+
+            <div className="med-list-card">
+                <div className="med-list-image"></div>
+
+                <div className="med-list-info">
+                    <p>name</p>
+                    <p>price</p>
+                    <div className="quantity">
+                        <label htmlFor="qty">Quantity</label>
+                        <input id="qty" type="number" />
+                    </div>
+                </div>
+
+                <div className="med-list-btn">
+                    <button id="add-to-cart">Add to checkout</button>
+                </div>
+            </div>
         </div>
     </>
   )
