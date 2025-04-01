9bff600cc92f4a698c623fa4e2ebe0b9436a1024
Uchenna99 <uchennaagbu@gmail.com>
2025-04-01 17:53:09 +0100

    starting history page

--- src/Components/History.tsx
+++ src/Components/History.tsx
@@ -4,7 +4,29 @@
 const History = () => {
   return (
     <>
-        History
+        <div className="history-section">
+            <div className="history-headers">
+                <div className="order-group-header">
+                    <p>All orders</p>
+                    <div className="underline-div"></div>
+                </div>
+
+                <div className="order-group-header">
+                    <p>Completed</p>
+                    <div className="underline-div"></div>
+                </div>
+
+                <div className="order-group-header">
+                    <p>Pending</p>
+                    <div className="underline-div"></div>
+                </div>
+
+                <div className="order-group-header">
+                    <p>Cancelled</p>
+                    <div className="underline-div"></div>
+                </div>
+            </div>
+        </div>
     </>
   )
 }
