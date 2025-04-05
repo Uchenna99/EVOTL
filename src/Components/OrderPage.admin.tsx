f432848ddb7fe3afd803be5e79984982c628d894
Uchenna99 <uchennaagbu@gmail.com>
2025-04-05 22:54:29 +0100

    adding admin services

--- src/Components/OrderPage.admin.tsx
+++ src/Components/OrderPage.admin.tsx
@@ -32,39 +32,48 @@ const OrderPage = () => {
   return (
     <>
         <div className="history-section">
-            <div className="history-headers">
-                <div className="order-group-header">
-                    <p onClick={()=> setSection('all')} style={{fontWeight:section === 'all'? 600:''}}>
-                        All orders
-                    </p>
-                    <div className="underline-div" style={{display: section === 'all'? 'block':'none'}}></div>
+            <div className="history-headers" style={{justifyContent:'space-between'}}>
+                <div className="history-headers-wrap">
+                    <div className="order-group-header">
+                        <p onClick={()=> setSection('all')} style={{fontWeight:section === 'all'? 600:''}}>
+                            All orders
+                        </p>
+                        <div className="underline-div" style={{display: section === 'all'? 'block':'none'}}></div>
+                    </div>
+
+                    <div className="order-group-header">
+                        <p onClick={()=> setSection('completed')} style={{fontWeight:section === 'completed'? 600:''}}>
+                            Completed
+                        </p>
+                        <div className="underline-div" style={{display: section === 'completed'? 'block':'none'}}></div>
+                    </div>
+
+                    <div className="order-group-header">
+                        <p onClick={()=> setSection('pending')} style={{fontWeight:section === 'pending'? 600:''}}>
+                            Pending
+                        </p>
+                        <div className="underline-div" style={{display: section === 'pending'? 'block':'none'}}></div>
+                    </div>
+
+                    <div className="order-group-header">
+                        <p onClick={()=> setSection('cancelled')} style={{fontWeight:section === 'cancelled'? 600:''}}>
+                            Cancelled
+                        </p>
+                        <div className="underline-div" style={{display: section === 'cancelled'? 'block':'none'}}></div>
+                    </div>
                 </div>
 
-                <div className="order-group-header">
-                    <p onClick={()=> setSection('completed')} style={{fontWeight:section === 'completed'? 600:''}}>
-                        Completed
-                    </p>
-                    <div className="underline-div" style={{display: section === 'completed'? 'block':'none'}}></div>
-                </div>
-
-                <div className="order-group-header">
-                    <p onClick={()=> setSection('pending')} style={{fontWeight:section === 'pending'? 600:''}}>
-                        Pending
-                    </p>
-                    <div className="underline-div" style={{display: section === 'pending'? 'block':'none'}}></div>
-                </div>
-
-                <div className="order-group-header">
-                    <p onClick={()=> setSection('cancelled')} style={{fontWeight:section === 'cancelled'? 600:''}}>
-                        Cancelled
-                    </p>
-                    <div className="underline-div" style={{display: section === 'cancelled'? 'block':'none'}}></div>
-                </div>
+                <button id="add-to-cart">
+                    Add New
+                </button>
             </div>
 
-                <div className="history-grid-header">
+                <div className="history-grid-header-2">
                     <div className="history-grid-header-label">
-                        Id
+                        Order Id
+                    </div>
+                    <div className="history-grid-header-label">
+                        User Id
                     </div>
                     <div className="history-grid-header-label">
                         Items (quantity)
@@ -84,36 +93,39 @@ const OrderPage = () => {
                 {
                     orders &&
                     orders.map((order, index)=> (
-                    <div className="history-item" key={index}>
-                        <div className="history-item-section">
-                            <p>#{shortenText(order.id, 10)}</p>
+                        <div className="history-item-2" key={index}>
+                            <div className="history-item-section">
+                                <p>#{shortenText(order.id, 10)}</p>
+                            </div>
+                            <div className="history-item-section">
+                                <p>{shortenText(order.userId, 10)}</p>
+                            </div>
+                            <div className="history-item-section">
+                                {
+                                    order.loads.map((load, index)=> (
+                                        <p key={index} style={{marginRight:5}}>
+                                            {medsNames?.map((med)=> med.id === load.medicationsId? med.name :'')}{' '}
+                                            ({load.quantity})
+                                            {index === order.loads.length-1 ? '':','}
+                                            {' '}
+                                        </p>
+                                    ))
+                                }
+                            </div>
+                            <div className="history-item-section">
+                                <p>{order.evtolId}</p>
+                            </div>
+                            <div className="history-item-section">
+                                <p style={{color: order.status === 'COMPLETED'? 'green':
+                                    order.status === 'CANCELLED'? 'red':''
+                                }}>
+                                    {order.status}
+                                </p>
+                            </div>
+                            <div className="history-item-section">
+                                <p>{new Date(order.createdAt).toLocaleString()}</p>
+                            </div>
                         </div>
-                        <div className="history-item-section">
-                            {
-                                order.loads.map((load, index)=> (
-                                    <p key={index} style={{marginRight:5}}>
-                                        {medsNames?.map((med)=> med.id === load.medicationsId? med.name :'')}{' '}
-                                        ({load.quantity})
-                                        {index === order.loads.length-1 ? '':','}
-                                        {' '}
-                                    </p>
-                                ))
-                            }
-                        </div>
-                        <div className="history-item-section">
-                            <p>{order.evtolId}</p>
-                        </div>
-                        <div className="history-item-section">
-                            <p style={{color: order.status === 'COMPLETED'? 'green':
-                                order.status === 'CANCELLED'? 'red':''
-                            }}>
-                                {order.status}
-                            </p>
-                        </div>
-                        <div className="history-item-section">
-                            <p>{new Date(order.createdAt).toLocaleString()}</p>
-                        </div>
-                    </div>
 
                     ))
                 }
--- src/Pages/AdminDashboard.tsx
+++ src/Pages/AdminDashboard.tsx
@@ -4,6 +4,8 @@ import { useState } from "react";
 import { IoMdNotificationsOutline } from "react-icons/io";
 import { DB_GetUser } from "../Components/interface";
 import { GoSearch } from "react-icons/go";
+import { LuHistory } from "react-icons/lu";
+import { FaHistory } from "react-icons/fa";
 import History from "../Components/History";
 import ProfilePage from "../Components/ProfilePage";
 import OrderPage from "../Components/OrderPage.admin";
@@ -38,45 +40,36 @@ const AdminDashboard = () => {
                 <h1 style={{color:'#EDF2F7'}}>
                 EVTOL
                 </h1>
-                {/* <div className="dash-user-image" style={{backgroundImage: `url(${user?.image})`}}></div>
-                <div className="dash-user-name">
-                <p>Hello</p>
-                <p>{`${user?.firstName} ${user?.lastName}`}</p>
-                </div> */}
             </div>
 
             <div className="dash-left-options">
 
                 <div className="dash-option-select" onClick={()=> setDashboard('orders')}
-                style={{animationName:dashboard==='orders'? 'select':''}}>
-                <p style={{color: dashboard==='orders'? '#5A67D8':'', fontWeight: dashboard === 'orders'? "bold" : '', fontSize:dashboard === 'orders'? 19 : ''}}>
-                    Orders
-                </p>
-                </div>
-
-                <div className="dash-option-select" onClick={()=> setDashboard('tracking')}
-                style={{animationName:dashboard==='tracking'? 'select':''}}>
-                <p style={{color: dashboard==='tracking'? '#5A67D8':'', fontWeight: dashboard === 'tracking'? "bold" : '', fontSize:dashboard === 'tracking'? 19 : ''}}>
-                    Order Tracking
-                </p>
+                    style={{animationName:dashboard==='orders'? 'select':''}}>
+                    <p style={{color: dashboard==='orders'? '#5A67D8':'', fontWeight: dashboard === 'orders'? "bold" : '', fontSize:dashboard === 'orders'? 19 : ''}}>
+                        Orders
+                    </p>
                 </div>
 
                 <div className="dash-option-select" onClick={()=> setDashboard('history')}
-                style={{animationName: dashboard==='history'? 'select':''}}>
-                <p style={{color: dashboard==='history'? '#5A67D8':'', fontWeight: dashboard === 'history'? "bold" : '', fontSize:dashboard === 'history'? 20 : ''}}>
-                    History
-                </p>
+                    style={{animationName: dashboard==='history'? 'select':''}}>
+                    <FaHistory id="dash-option-icon"
+                        style={{color: dashboard === 'history'? '#5A67D8':''}}
+                    />
+                    <p style={{color: dashboard==='history'? '#5A67D8':'', fontWeight: dashboard === 'history'? "bold" : '', fontSize:dashboard === 'history'? 20 : ''}}>
+                        History
+                    </p>
                 </div>
 
                 <div className="dash-option-select" onClick={()=> setDashboard('profile')}
-                style={{animationName:dashboard==='profile'? 'select':''}}>
-                <p style={{color: dashboard==='profile'? '#5A67D8':'', fontWeight: dashboard === 'profile'? "bold" : '', fontSize:dashboard === 'profile'? 19 : ''}}>
-                    Profile
-                </p>
+                    style={{animationName:dashboard==='profile'? 'select':''}}>
+                    <p style={{color: dashboard==='profile'? '#5A67D8':'', fontWeight: dashboard === 'profile'? "bold" : '', fontSize:dashboard === 'profile'? 19 : ''}}>
+                        Profile
+                    </p>
                 </div>
 
                 <div className="dash-option-select" onClick={handleLogout}>
-                <p>Logout</p>
+                    <p>Logout</p>
                 </div>
             </div>
             </div>
